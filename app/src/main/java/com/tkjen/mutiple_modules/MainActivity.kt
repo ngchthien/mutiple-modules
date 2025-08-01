09610c30d3706b69331c742703b7967cea262500
Chi Thien Ngo <thienngo2003@gmail.com>
2025-08-01 15:44:32 +0700

    first commit

---
+++ app/src/main/java/com/tkjen/mutiple_modules/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tkjen.mutiple_modules
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
