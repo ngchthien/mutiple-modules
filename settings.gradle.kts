d500da5a98526a11873633e38b938f4523ef5800
Chi Thien Ngo <thienngo2003@gmail.com>
2025-08-01 16:07:12 +0700

    create empty modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "mutiple-modules"
 include(":app")
+include(":core-network")
+include(":core-localstorage")
+include(":core-model")
+include(":core-data")
+include(":core-theme")
+include(":core-designSystem")
+include(":core-navigation")
